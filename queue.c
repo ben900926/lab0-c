85b1aff85ca04d3d7228bb3f514f61a25f070906
ben900926 <ben900926.cs09@nycu.edu.tw>
2024-02-23 01:19:12 +0800

    Implement new and free functions

--- queue.c
+++ queue.c
@@ -14,11 +14,25 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *head = malloc(sizeof(struct list_head));
+    if (!head)
+        return NULL;
+
+    INIT_LIST_HEAD(head);
+    return head;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    element_t *entry, *safe;
+    list_for_each_entry_safe (entry, safe, l, list)
+        if (entry->value)
+            free(entry->value);
+    free(entry);
+
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
