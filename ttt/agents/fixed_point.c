fc4a2c6cc62a2a1cf50c9543a146134cef6d90ee
ben900926 <ben900926.cs09@nycu.edu.tw>
2024-03-24 10:56:51 +0800

    Implement fix pointed math functions
    log, square functions are implemented with fixed-point arthimetic

--- ttt/agents/fixed_point.c
+++ ttt/agents/fixed_point.c
@@ -15,11 +15,11 @@
  * of course trivially computable in O(log_2 n), the length of our binary
  * vector.
  */
-static fixed_point_t fixed_power_int(fixed_point_t x,
+static unsigned long fixed_power_int(unsigned long x,
                                      unsigned int frac_bits,
                                      unsigned int n)
 {
-    fixed_point_t result = 1UL << frac_bits;
+    unsigned long result = 1UL << frac_bits;
 
     if (n) {
         for (;;) {
@@ -42,38 +42,102 @@ static fixed_point_t fixed_power_int(fixed_point_t x,
 
 static void print_fixed(fixed_point_t x)
 {
-    printf("%lu.%02lu\n", LOAD_INT(x), LOAD_FRAC(x));
+    printf("%ld.%02ld\n", LOAD_INT(x), LOAD_FRAC(x));
 }
 
 /**
  * return a * b in fixed-point format
  */
-fixed_point_t multi(fixed_point_t a, fixed_point_t b)
+fixed_point_t multi_f(fixed_point_t a, fixed_point_t b)
 {
-    fixed_point_t result = a * b;
+    unsigned long result = a * b;
     result += 1UL << (FSHIFT - 1);
     result >>= FSHIFT;
+
     return result;
 }
 /**
  * return a / b in fixed-point
  * take scale factor so fractions of the result can be kept
  */
-fixed_point_t div(fixed_point_t a, fixed_point_t b)
+fixed_point_t div_f(fixed_point_t a, fixed_point_t b)
 {
-    fixed_point_t result = (a << SCALE_FACTOR) / b;
+    unsigned long result = (a << SCALE_FACTOR) / b;
     result = LOAD_FIXED(result);
     result >>= SCALE_FACTOR;
+
     return result;
 }
 
 
+/**
+ * using divison-free approximation of sqrt
+ * ref: https://hackmd.io/@vax-r/linux2024-homework1#Monte-Carlo-Search-Tree
+ */
+fixed_point_t sqrt_f(fixed_point_t num)
+{
+    fixed_point_t res = 0L;
+    /* i is intialized as the index of first bit 1 in num */
+    /* t starts from num, divided by two each time */
+    for (int i = 31 - __builtin_clz(num | 1); i >= 0; i--) {
+        fixed_point_t t = 1UL << i;
+        if (multi_f(res + t, res + t) <= num)
+            res += t;
+    }
+    return res;
+}
+
+/**
+ * using recursion to find log2 first
+ * ref: https://github.com/dmoulding/log2fix/blob/master/log2fix.c
+ */
+fixed_point_t log2_f(fixed_point_t num)
+{
+    if (num <= 0)
+        return INT32_MIN;
+
+    /* b = 1/2 */
+    fixed_point_t b = LOAD_FIXED(1L) >> 1;
+    fixed_point_t y = 0;
+
+    /* make num in [1, 2) */
+    while (num < LOAD_FIXED(1L)) {
+        num <<= 1;
+        y -= LOAD_FIXED(1L);
+    }
+    while (num >= LOAD_FIXED(2L)) {
+        num >>= 1;
+        y += LOAD_FIXED(1L);
+    }
+
+    fixed_point_t z = num;
+    for (size_t i = 0; i < FSHIFT; i++) {
+        z = multi_f(z, z);
+        /* if x >= 2: x = x/2, y = y+b */
+        if (z >= LOAD_FIXED(2L)) {
+            z >>= 1;
+            y += b;
+        }
+        b >>= 1;
+    }
+
+    return y;
+}
+
+fixed_point_t loge_f(fixed_point_t num)
+{
+    uint64_t t = log2_f(num) * INV_LOG2_E;
+    return t >> 31;
+}
+
 int main(void)
 {
-    fixed_point_t x = 0b01010110 << (FSHIFT - 4);
-    fixed_point_t y = 0b00010110 << (FSHIFT - 4);
+    fixed_point_t x = LOAD_FIXED(0b01010010);
+    fixed_point_t y = LOAD_FIXED(0b011010010);
     print_fixed(x);
     print_fixed(y);
-    y = div(x, y);
-    printf("x / y = %lu.%lu\n", LOAD_INT(y), LOAD_FRAC(y));
+    y = multi_f(x, y);
+    printf("x / y = %ld.%ld\n", LOAD_INT(y), LOAD_FRAC(y));
+    print_fixed(sqrt_f(LOAD_FIXED(512L)));
+    print_fixed(loge_f(LOAD_FIXED(512L)));
 }
--- ttt/agents/fixed_point.h
+++ ttt/agents/fixed_point.h
@@ -1,6 +1,8 @@
 #ifndef LAB0_FIXEDPOINT_H
 #define LAB0_FIXEDPOINT_H
 
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 /* Linux-kernel fixed point arthimetic implementation */
@@ -12,7 +14,13 @@
 #define LOAD_INT(x) ((x) >> FSHIFT)
 #define LOAD_FRAC(x) \
     LOAD_INT(((x) & (FIXED_1 - 1)) * 100) /* keep 2 digits of fractions */
+
 #define SCALE_FACTOR 3 /* this is used to keep the fractions of division */
-typedef unsigned long fixed_point_t;
+
+#define INV_LOG2_E 0x58b90bfc /* Inverse log base 2 of e */
+
+typedef long fixed_point_t;
+
+
 
 #endif
